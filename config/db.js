479d08f4e3bfa1dbcabb2ee6cb5364752cdbecaa
Pavan Kalyan <paidipellipavan@gmail.com>
2025-07-20 15:09:40 +0530

    Remove db.js from config and added in .gitignore

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://paidipellipavan:S7lIcB7LrlP13o4A@cluster0.2h9pi0k.mongodb.net/Food_Delivery').then(()=>console.log('DB connected Successfully '));
-    
-}
